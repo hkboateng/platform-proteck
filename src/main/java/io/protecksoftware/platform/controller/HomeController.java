a27c4cfec8ee9114575d6382b69f9530277268e9
Kyeremateng <hubert.kyeremateng@accenturefederal.com>
2020-02-24 11:58:42 -0500

    added ui template files

---
+++ src/main/java/io/protecksoftware/platform/controller/HomeController.java
@@ -0,0 +1,19 @@
+package io.protecksoftware.platform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String index() {
+		return "public/index";
+	}
+	
+	@RequestMapping(value="/demo",method=RequestMethod.GET)
+	public String demo() {
+		return "public/demo";
+	}
+}
