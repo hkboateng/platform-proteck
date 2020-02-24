19831aeecc9002bee7ec08098ef1b748aadc718b
Kyeremateng <hubert.kyeremateng@accenturefederal.com>
2020-02-24 10:14:54 -0500

    Added project docs

---
+++ src/test/java/io/protecksoftware/platform/ProteckPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.protecksoftware.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteckPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
